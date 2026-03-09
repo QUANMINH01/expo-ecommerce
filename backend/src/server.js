4c8bd14710d4b856923634c54874b2db720608f9
minhquan <minhquan123dn02@gmail.com>
2026-03-09 17:08:00 +0700

    startServer method added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path";
 import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { start } from "repl";
 
 const app = express();
 
@@ -22,7 +23,12 @@ if (ENV.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
   });
 }
-app.listen(ENV.PORT, () => {
-  console.log("Server is up and running");
-  connectDB();
-});
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log(`Server is up and running`);
+  });
+};
+
+startServer();
