6368d2ac21f95d4a24563a3b9e93338b65a80b5e
minhquan <minhquan123dn02@gmail.com>
2026-03-12 15:16:11 +0700

    auth implemented

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { ClerkProvider } from "@clerk/react";
+
+const PUSLISTABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUSLISTABLE_KEY) {
+  throw new Error("Missing publishable key");
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUSLISTABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
